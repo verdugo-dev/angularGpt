c1106d4015fd53f1e28d7b4e8815f335c0142ade
Carlos Verdugo <carlos@adroitoverseas.com>
2026-04-13 20:31:31 -0700

    adding typing loader

---
+++ src/app/presentation/components/typingLoader/typingLoader.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-loader',
+  imports: [],
+  templateUrl: './typingLoader.html',
+  styleUrl: './typingLoader.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TypingLoader { }
--- src/app/presentation/pages/ortographyPage/ortographyPage.ts
+++ src/app/presentation/pages/ortographyPage/ortographyPage.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatMessage } from '@components/chat-bubbles/chatMessage/chatMessage';
 import { MyMessage } from '@components/chat-bubbles/myMessage/myMessage';
+import { TypingLoader } from '@components/typingLoader/typingLoader';
 
 @Component({
   selector: 'app-ortography-page',
-  imports: [ChatMessage, MyMessage],
+  imports: [ChatMessage, MyMessage, TypingLoader],
   templateUrl: './ortographyPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
