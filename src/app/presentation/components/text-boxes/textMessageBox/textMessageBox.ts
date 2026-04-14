2a001f30a14ec70a3d6dacc253b4836f9e537ded
Carlos Verdugo <carlos@adroitoverseas.com>
2026-04-13 20:46:02 -0700

    Adding text message box

---
+++ src/app/presentation/components/text-boxes/textMessageBox/textMessageBox.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-text-message-box',
+  imports: [ReactiveFormsModule],
+  templateUrl: './textMessageBox.html',
+  styleUrl: './textMessageBox.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessageBox {
+
+  @Input() placeholder: string = "";
+  @Input() disableCorrectrions: boolean = false;
+
+  @Output() onMessage = new EventEmitter<string>();
+
+
+  public fb = inject(FormBuilder);
+  public form = this.fb.group({
+    prompt: ["", Validators.required]
+  });
+
+  handleSubmit() {
+    if (this.form.invalid) return;
+
+    const {prompt} = this.form.value;
+    console.log({prompt})
+
+    this.onMessage.emit( prompt ?? "");
+    this.form.reset();
+  }
+
+}
--- src/app/presentation/pages/ortographyPage/ortographyPage.ts
+++ src/app/presentation/pages/ortographyPage/ortographyPage.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatMessage } from '@components/chat-bubbles/chatMessage/chatMessage';
 import { MyMessage } from '@components/chat-bubbles/myMessage/myMessage';
+import { TextMessageBox } from '@components/text-boxes/textMessageBox/textMessageBox';
 import { TypingLoader } from '@components/typingLoader/typingLoader';
 
 @Component({
   selector: 'app-ortography-page',
-  imports: [ChatMessage, MyMessage, TypingLoader],
+  imports: [ChatMessage, MyMessage, TypingLoader, TextMessageBox],
   templateUrl: './ortographyPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
