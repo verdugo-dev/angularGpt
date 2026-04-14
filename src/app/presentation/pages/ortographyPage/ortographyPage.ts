f84d66f115637e1ed961037420462d0527ca94ee
Carlos Verdugo <carlos@adroitoverseas.com>
2026-04-13 21:35:11 -0700

    Adding text message interface

--- src/app/presentation/pages/ortographyPage/ortographyPage.ts
+++ src/app/presentation/pages/ortographyPage/ortographyPage.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ChatMessage } from '@components/chat-bubbles/chatMessage/chatMessage';
 import { MyMessage } from '@components/chat-bubbles/myMessage/myMessage';
 import { TextMessageBox } from '@components/text-boxes/textMessageBox/textMessageBox';
 import { TextMessageBoxFile, TextMessageEvent } from '@components/text-boxes/textMessageBoxFile/textMessageBoxFile';
 import { TextMessageBoxEvent, TextMessageBoxSelect } from '@components/text-boxes/textMessageBoxSelect/textMessageBoxSelect';
 import { TypingLoader } from '@components/typingLoader/typingLoader';
+import { Message } from 'src/app/interfaces/message.interface';
 
 @Component({
   selector: 'app-ortography-page',
@@ -13,6 +14,9 @@ import { TypingLoader } from '@components/typingLoader/typingLoader';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrtographyPage {
+  public messages = signal<Message[]>([{text: 'Hola mundo', isGpt: false }]);
+  public isLoading = signal(false);
+
   handleMessage(prompt: string) {
     console.log({prompt})
   }
